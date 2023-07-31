f45fbd212d207158bd041149fe086dc0f9916628
Shivansh270 <gupta.shivansh2709@gmail.com>
2023-07-31 23:48:20 +0530

    updation in app.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -23,32 +23,6 @@ function App() {
     return data;
 };
 
-const autoTypingBotResponse = (text) => {
-  let index = 0;
-  let interval = setInterval(() => {
-      if (index < text.length) {
-          setPosts((prevState) => {
-              let lastItem = prevState.pop();
-              if (lastItem.type !== "bot") {
-                  prevState.push({
-                      type: "bot",
-                      post: text.charAt(index - 1),
-                  });
-              } else {
-                  prevState.push({
-                      type: "bot",
-                      post: lastItem.post + text.charAt(index - 1),
-                  });
-              }
-              return [...prevState];
-          });
-          index++;
-      } else {
-          clearInterval(interval);
-      }
-  }, 20);
-};
-
   const clickHandler = () => {
     if(input.trim() === '') return
     updatePosts(input)
