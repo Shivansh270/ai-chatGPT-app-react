a6f01fba5c88390143e850558f09c89fbe8139f9
Shivansh270 <gupta.shivansh2709@gmail.com>
2023-07-31 23:44:47 +0530

    updation in app.js

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,40 +2,107 @@ import { useState } from 'react'
 import send from "./assets/send.svg"
 import user from "./assets/user.png"
 import loader from "./assets/loader.svg"
-// import bot from "./assets/send.svg"
+import bot from "./assets/bot.png" 
+import axios from "axios"
 
 function App() {
 
   const[input, setInput] = useState('');
   const[posts,setPosts] = useState([])
 
-  const clickHandler = () => {
+  const fetchBotResponse = async () => {
+    const { data } = await axios.post(
+        "http://localhost:4000/",
+        {input},
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+    );
+    return data;
+};
 
+const autoTypingBotResponse = (text) => {
+  let index = 0;
+  let interval = setInterval(() => {
+      if (index < text.length) {
+          setPosts((prevState) => {
+              let lastItem = prevState.pop();
+              if (lastItem.type !== "bot") {
+                  prevState.push({
+                      type: "bot",
+                      post: text.charAt(index - 1),
+                  });
+              } else {
+                  prevState.push({
+                      type: "bot",
+                      post: lastItem.post + text.charAt(index - 1),
+                  });
+              }
+              return [...prevState];
+          });
+          index++;
+      } else {
+          clearInterval(interval);
+      }
+  }, 20);
+};
+
+  const clickHandler = () => {
+    if(input.trim() === '') return
+    updatePosts(input)
+    updatePosts("loading...", false, true);
+    setInput("");
+    fetchBotResponse().then((res) => {
+      console.log(res.bot.trim());
+      updatePosts(res.bot.trim(), true);
+    });
+  }
+
+  const updatePosts = (post, isBot, isLoading) => {
+    if (isBot) {
+        autoTypingBotResponse(post);
+    } else{
+    setPosts(prevData => {
+      return [
+        ...prevData,
+        {type: isLoading ? "loading" : "user", post}
+      ]
+    })
+  } 
+ } 
+
+  const onKeyUp = (event) => {
+      if(event.key == "Enter")
+      clickHandler()
   }
 
   return (
     <div className='app'>
       <section className='chat-container'>
+        <h1 className='heading'>TalkGenius</h1>
         <div className='layout'>
 
-          {posts.map((post, index) => (
+          {posts?.map((post, index) => (
 
-            <div className={`chat-bubble ${post.type === 'bot' || post.type ==='loading'
-              ? 'bot' : ''}`}>
+            <div key={index}
+             className={`chat-bubble ${post.type === "bot" || post.type ==="loading"
+              ? "bot" : ""}`}> 
 
               <div className='avatar'>
-                <img src={post.type === 'bot' || post.type ==='loading'
-                ? 'bot' : 'user'} />
+                <img src={post.type === "bot" || post.type ==="loading"
+                ? {bot} : {user}} />
               </div>
 
               {post.type === 'loading' ? (
                 <div className='loader'>
                   <img src={loader} />
                 </div>
-                ) : <div className='post'>{post.post}</div>
-              }
+                ) : (<div className='post'>{post.post}</div>
+              )}
 
-            </div>
+            </div> 
           ))}
 
         </div>
@@ -43,11 +110,13 @@ function App() {
 
       <footer>
         <input
+          value={input}
           className='composebar'
           autoFocus
           type='text'
           placeholder='Send a message...'
           onChange={(event) => setInput(event.target.value)}
+          onKeyUp={onKeyUp}
          />
 
         <div className='send-button' onClick={clickHandler}>
